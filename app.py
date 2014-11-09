4b439c986a79e95562b273dff43537ab5d659fc0
justinstrauss <justinianstrauss@gmail.com>
2014-11-08 21:09:15 -0500

    yay, progress

--- app.py
+++ app.py
@@ -3,13 +3,41 @@
 # API Project
 
 from flask import Flask, render_template, request, redirect, session, url_for, flash
+import urllib2
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=["POST","GET"])
 @app.route('/index', methods=["POST","GET"])
 def index():
-    return render_template("index.html")
+	if request.method=="POST":
+		origin = request.form["origin"]
+		destination = request.form["destination"]
+		station1 = getClosest(origin)
+		station2 = getClosest(origin)
+		return render_template("result.html")
+	else:
+		return render_template("index.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+def getClosest(address):
+# returns the closest Citibike Station to a given address
+	url = "http://www.citibikenyc.com/stations/json"
+	request = urllib2.urlopen(url)
+	result = request.read()
+	d = json.loads(result)
+	rlist = d['stationBeanList']
+	stationlist = [r["stationName"] for r in rlist]
+	print stationlist
+	return address
+
+# @app.route('/authorize')
+# def authorize():
+# 	return
 
 if __name__ == '__main__':
     app.debug = True
