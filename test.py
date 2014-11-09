ec53eaaa479309a0925bb90d698efddaf0e5d95f
Angela Lin <angela97lin@gmail.com>
2014-11-08 21:15:50 -0500

    just changing base into citibike info

--- test.py
+++ test.py
@@ -2,11 +2,13 @@ import urllib2
 import json
 
 if __name__ == '__main__':
-	url = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s&safe=on"
-	url = url%("narwhal")
+        url = "http://www.citibikenyc.com/stations/json"
 	request = urllib2.urlopen(url)
 	result = request.read()
-	d = json.loads(result)
-	rlist = d['responseData']['results']
+	d = json.loads(result) #dictionary of our results (tuples?)
+        #print d
+        execTime = d['executionTime'] #gets the time when program was run, keeping here in case we want to use that somewhere!
+        #just for testing purposes...
+	rlist = d['stationBeanList']
 	for r in rlist:
-		print r['titleNoFormatting']
+		print r['stationName'] 
